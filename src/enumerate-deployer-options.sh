316179b013c6265460cffd01a3276fcc0303796a
Allen D. Householder <adh@cert.org>
2023-06-14 15:28:11 -0400

    fixup variable name in generator script

--- src/enumerate-deployer-options.sh
+++ src/enumerate-deployer-options.sh
@@ -18,8 +18,8 @@ echo "row,Exploitation,Exposure,Automatable,HumanImpact,Priority(TODO)">$out
 for Exploitation in none PoC active
   do for Exposure in small controlled open
     do for Automatable in no yes
-      do for WellnessMissionImpact in low medium high "very high"
-          do echo $i,$Exploitation,$Exposure,$Automatable,$WellnessMissionImpact,>>$out
+      do for HumanImpact in low medium high "very high"
+          do echo $i,$Exploitation,$Exposure,$Automatable,$HumanImpact,>>$out
              i=$(($i+1))
       done
     done
