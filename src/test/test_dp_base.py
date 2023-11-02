dddc0b5c206943a75d5920564677234e753ba17c
Allen D. Householder <adh@cert.org>
2023-11-02 15:53:52 -0400

    make decision points base rountrip to-from json correctly
    - It was broken before (values were not getting created as objects)
    - Make the _Commented a mixin class, but remove it from the base
    class anyway. We don't really need it yet.
    - Simplify the decision point registry
    - fix up unit tests

--- src/test/test_dp_base.py
+++ src/test/test_dp_base.py
@@ -1,12 +1,33 @@
+#  Copyright (c) 2023 Carnegie Mellon University and Contributors.
+#  - see Contributors.md for a full list of Contributors
+#  - see ContributionInstructions.md for information on how you can Contribute to this project
+#  Stakeholder Specific Vulnerability Categorization (SSVC) is
+#  licensed under a MIT (SEI)-style license, please see LICENSE.md distributed
+#  with this Software or contact permission@sei.cmu.edu for full terms.
+#  Created, in part, with funding and support from the United States Government
+#  (see Acknowledgments file). This program may include and/or can make use of
+#  certain third party source code, object code, documentation and other files
+#  (“Third Party Software”). See LICENSE.md for more details.
+#  Carnegie Mellon®, CERT® and CERT Coordination Center® are registered in the
+#  U.S. Patent and Trademark Office by Carnegie Mellon University
+
 import unittest
+
 import ssvc.decision_points.base as base
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.value = base.SsvcDecisionPointValue(
-            name="foo", key="bar", description="baz"
-        )
+        self.original_registry = base.REGISTERED_DECISION_POINTS.copy()
+
+        # add multiple values
+        self.values = []
+        for i in range(3):
+            self.values.append(
+                base.SsvcDecisionPointValue(
+                    name=f"foo{i}", key=f"bar{i}", description=f"baz{i}"
+                )
+            )
 
         self.dp = base.SsvcDecisionPoint(
             name="foo",
@@ -14,19 +35,40 @@ class MyTestCase(unittest.TestCase):
             description="baz",
             version="1.0.0",
             namespace="ns",
-            values=(self.value,),
+            values=tuple(self.values),
         )
 
-    def test_ssvc_value(self):
-        obj = self.value
-        # should have name, key, description
-        self.assertEqual(obj.name, "foo")
-        self.assertEqual(obj.key, "bar")
-        self.assertEqual(obj.description, "baz")
+    def tearDown(self) -> None:
+        # restore the original registry
+        base.REGISTERED_DECISION_POINTS = self.original_registry
 
-        # should not have namespace, version
-        self.assertFalse(hasattr(obj, "namespace"))
-        self.assertFalse(hasattr(obj, "version"))
+    def test_registry(self):
+        # just by creating the objects, they should be registered
+        self.assertIn(self.dp, base.REGISTERED_DECISION_POINTS)
+
+        dp2 = base.SsvcDecisionPoint(
+            name="asdfad",
+            key="asdfasdf",
+            description="asdfasdf",
+            version="1.33.1",
+            namespace="asdfasdf",
+            values=tuple(self.values),
+        )
+
+        dp2._comment = "asdfasdfasdf"
+
+        self.assertIn(dp2, base.REGISTERED_DECISION_POINTS)
+
+    def test_ssvc_value(self):
+        for i, obj in enumerate(self.values):
+            # should have name, key, description
+            self.assertEqual(obj.name, f"foo{i}")
+            self.assertEqual(obj.key, f"bar{i}")
+            self.assertEqual(obj.description, f"baz{i}")
+
+            # should not have namespace, version
+            self.assertFalse(hasattr(obj, "namespace"))
+            self.assertFalse(hasattr(obj, "version"))
 
     def test_ssvc_decision_point(self):
         obj = self.dp
@@ -36,17 +78,16 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(obj.description, "baz")
         self.assertEqual(obj.version, "1.0.0")
         self.assertEqual(obj.namespace, "ns")
-        self.assertEqual(len(obj.values), 1)
+        self.assertEqual(len(self.values), len(obj.values))
 
     def test_ssvc_value_json_roundtrip(self):
-        obj = self.value
+        for i, obj in enumerate(self.values):
+            json = obj.to_json()
+            self.assertIsInstance(json, str)
+            self.assertGreater(len(json), 0)
 
-        json = obj.to_json()
-        self.assertIsInstance(json, str)
-        self.assertGreater(len(json), 0)
-
-        obj2 = base.SsvcDecisionPointValue.from_json(json)
-        self.assertEqual(obj, obj2)
+            obj2 = base.SsvcDecisionPointValue.from_json(json)
+            self.assertEqual(obj, obj2)
 
     def test_ssvc_decision_point_json_roundtrip(self):
         obj = self.dp
@@ -56,7 +97,10 @@ class MyTestCase(unittest.TestCase):
         self.assertGreater(len(json), 0)
 
         obj2 = base.SsvcDecisionPoint.from_json(json)
+
+        # the objects should be equal
         self.assertEqual(obj, obj2)
+        self.assertEqual(obj.to_dict(), obj2.to_dict())
 
 
 if __name__ == "__main__":
