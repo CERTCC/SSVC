2c7bd87689af7dd2fb494be3291d6c5b8d3be82b
Allen D. Householder <adh@cert.org>
2023-11-02 13:00:15 -0400

    add previously unrepresented decision points

---
+++ docs/reference/decision_points/human_impact.md
@@ -0,0 +1,8 @@
+# Human Impact
+
+{% include-markdown "../../_generated/decision_points/human_impact.md" %}
+
+!!! tip "See also"
+
+    Human Impact is a combination of [Safety Impact](./safety_impact.md) and
+    [Mission Impact](./mission_impact.md)
--- src/ssvc/decision_points/virulence.py
+++ src/ssvc/decision_points/virulence.py
@@ -4,18 +4,33 @@ file: virulence
 author: adh
 created_at: 9/21/23 9:58 AM
 """
+#  Copyright (c) 2023 Carnegie Mellon University and Contributors.
+#  - see Contributors.md for a full list of Contributors
+#  - see ContributionInstructions.md for information on how you can Contribute to this project
+#  Stakeholder Specific Vulnerability Categorization (SSVC) is
+#  licensed under a MIT (SEI)-style license, please see LICENSE.md distributed
+#  with this Software or contact permission@sei.cmu.edu for full terms.
+#  Created, in part, with funding and support from the United States Government
+#  (see Acknowledgments file). This program may include and/or can make use of
+#  certain third party source code, object code, documentation and other files
+#  (“Third Party Software”). See LICENSE.md for more details.
+#  Carnegie Mellon®, CERT® and CERT Coordination Center® are registered in the
+#  U.S. Patent and Trademark Office by Carnegie Mellon University
+
 from ssvc.decision_points.base import SsvcDecisionPoint, SsvcDecisionPointValue
 
 RAPID = SsvcDecisionPointValue(
     name="Rapid",
     key="R",
-    description="Steps 1-4 of the of the kill chain can be reliably automated. If the vulnerability allows remote code execution or command injection, the default response should be rapid.",
+    description="Steps 1-4 of the of the kill chain can be reliably automated. If the vulnerability allows remote "
+    "code execution or command injection, the default response should be rapid.",
 )
 
 SLOW = SsvcDecisionPointValue(
     name="Slow",
     key="S",
-    description="Steps 1-4 of the kill chain cannot be reliably automated for this vulnerability for some reason. These steps are reconnaissance, weaponization, delivery, and exploitation. Example reasons for why a step may not be reliably automatable include (1) the vulnerable component is not searchable or enumerable on the network, (2) weaponization may require human direction for each target, (3) delivery may require channels that widely deployed network security configurations block, and (3) exploitation may be frustrated by adequate exploit-prevention techniques enabled by default; ASLR is an example of an exploit-prevention tool.",
+    description="Steps 1-4 of the kill chain cannot be reliably automated for this vulnerability for some reason. "
+    "These steps are reconnaissance, weaponization, delivery, and exploitation.",
 )
 
 VIRULENCE_1 = SsvcDecisionPoint(
