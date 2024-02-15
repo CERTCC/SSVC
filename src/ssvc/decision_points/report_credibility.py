d7e86b57695653e93dc07bf4077af682125b71db
Allen D. Householder <adh@cert.org>
2024-02-15 15:47:31 -0500

    fix ordering of decision point values

--- src/ssvc/decision_points/report_credibility.py
+++ src/ssvc/decision_points/report_credibility.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 """
-file: report_credibility
-author: adh
-created_at: 9/21/23 11:24 AM
+Provides the SSVC Report Credibility Decision Point
 """
+
+#  Copyright (c) 2023-2024 Carnegie Mellon University and Contributors.
+#  - see Contributors.md for a full list of Contributors
+#  - see ContributionInstructions.md for information on how you can Contribute to this project
+#  Stakeholder Specific Vulnerability Categorization (SSVC) is
+#  licensed under a MIT (SEI)-style license, please see LICENSE.md distributed
+#  with this Software or contact permission@sei.cmu.edu for full terms.
+#  Created, in part, with funding and support from the United States Government
+#  (see Acknowledgments file). This program may include and/or can make use of
+#  certain third party source code, object code, documentation and other files
+#  (“Third Party Software”). See LICENSE.md for more details.
+#  Carnegie Mellon®, CERT® and CERT Coordination Center® are registered in the
+#  U.S. Patent and Trademark Office by Carnegie Mellon University
 
 from ssvc.decision_points.base import SsvcDecisionPoint, SsvcDecisionPointValue
 
@@ -25,8 +36,8 @@ REPORT_CREDIBILITY_1 = SsvcDecisionPoint(
     key="RC",
     version="1.0.0",
     values=(
-        CREDIBLE,
         NOT_CREDIBLE,
+        CREDIBLE,
     ),
 )
 
--- src/ssvc/decision_points/report_public.py
+++ src/ssvc/decision_points/report_public.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 """
-file: report_public
-author: adh
-created_at: 9/21/23 11:15 AM
+Provides the SSVC Report Public Decision Point
 """
+#  Copyright (c) 2023-2024 Carnegie Mellon University and Contributors.
+#  - see Contributors.md for a full list of Contributors
+#  - see ContributionInstructions.md for information on how you can Contribute to this project
+#  Stakeholder Specific Vulnerability Categorization (SSVC) is
+#  licensed under a MIT (SEI)-style license, please see LICENSE.md distributed
+#  with this Software or contact permission@sei.cmu.edu for full terms.
+#  Created, in part, with funding and support from the United States Government
+#  (see Acknowledgments file). This program may include and/or can make use of
+#  certain third party source code, object code, documentation and other files
+#  (“Third Party Software”). See LICENSE.md for more details.
+#  Carnegie Mellon®, CERT® and CERT Coordination Center® are registered in the
+#  U.S. Patent and Trademark Office by Carnegie Mellon University
+
 from ssvc.decision_points.base import SsvcDecisionPoint, SsvcDecisionPointValue
 
 YES = SsvcDecisionPointValue(
@@ -24,8 +35,8 @@ REPORT_PUBLIC_1 = SsvcDecisionPoint(
     key="RP",
     version="1.0.0",
     values=(
-        NO,
         YES,
+        NO,
     ),
 )
 
