a9a07e7e72ae2c6593208a901fde9b677f4e7c4a
Allen D. Householder <adh@cert.org>
2024-02-21 12:44:32 -0500

    fix order of public value added decision point

--- src/ssvc/decision_points/public_value_added.py
+++ src/ssvc/decision_points/public_value_added.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 """
-file: public_value_added
-author: adh
-created_at: 9/21/23 11:27 AM
+This module provides the Public Value Added decision point for the Stakeholder Specific Vulnerability Categorization (SSVC) framework.
 """
 
+#  Copyright (c) 2024 Carnegie Mellon University and Contributors.
+#  - see Contributors.md for a full list of Contributors
+#  - see ContributionInstructions.md for information on how you can Contribute to this project
+#  Stakeholder Specific Vulnerability Categorization (SSVC) is
+#  licensed under a MIT (SEI)-style license, please see LICENSE.md distributed
+#  with this Software or contact permission@sei.cmu.edu for full terms.
+#  Created, in part, with funding and support from the United States Government
+#  (see Acknowledgments file). This program may include and/or can make use of
+#  certain third party source code, object code, documentation and other files
+#  (“Third Party Software”). See LICENSE.md for more details.
+#  Carnegie Mellon®, CERT® and CERT Coordination Center® are registered in the
+#  U.S. Patent and Trademark Office by Carnegie Mellon University
+
 from ssvc.decision_points.base import SsvcDecisionPoint, SsvcDecisionPointValue
+from ssvc.decision_points.helpers import print_versions_and_diffs
 
 LIMITED = SsvcDecisionPointValue(
     name="Limited",
@@ -30,12 +42,14 @@ PUBLIC_VALUE_ADDED_1 = SsvcDecisionPoint(
     description="How much value would a publication from the coordinator benefit the broader community?",
     key="PVA",
     version="1.0.0",
-    values=(PRECEDENCE, AMPLIATIVE, LIMITED),
+    values=(LIMITED, AMPLIATIVE, PRECEDENCE),
 )
 
 
 def main():
-    print(PUBLIC_VALUE_ADDED_1.to_json(indent=2))
+    versions = (PUBLIC_VALUE_ADDED_1,)
+
+    print_versions_and_diffs(versions)
 
 
 if __name__ == "__main__":
