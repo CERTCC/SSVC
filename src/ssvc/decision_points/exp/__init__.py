d7b68b5acdb96572e5c9ad1ae5aba0338e0e434c
Allen D. Householder <adh@cert.org>
2024-06-27 15:09:27 -0400

    clean up cruft in __init__.py

--- src/ssvc/decision_points/exp/__init__.py
+++ src/ssvc/decision_points/exp/__init__.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-"""
-file: __init__.py
-author: adh
-created_at: 6/27/24 2:56 PM
-"""
-
-
 #  Copyright (c) 2024 Carnegie Mellon University and Contributors.
 #  - see Contributors.md for a full list of Contributors
 #  - see ContributionInstructions.md for information on how you can Contribute to this project
@@ -18,11 +10,3 @@ created_at: 6/27/24 2:56 PM
 #  (“Third Party Software”). See LICENSE.md for more details.
 #  Carnegie Mellon®, CERT® and CERT Coordination Center® are registered in the
 #  U.S. Patent and Trademark Office by Carnegie Mellon University
-
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
