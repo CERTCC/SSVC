0f85e1ebc633e0a75ed0621a4998bdf99f4d85e2
Vijay Sarvepalli <vssarvepalli@cert.org>
2024-07-16 13:22:41 -0400

    Resolve part of Feature/issue 602 to add schemaVersion field (#606)
    * Updates to schema to add schemaVersion as optional field
    
    * Updated all data/json/decision_point/* files appropriately with schemaVersion field
    
    * run doctools.py on the current code
    
    ---------
    
    Co-authored-by: Allen D. Householder <ahouseholder@users.noreply.github.com>
    Co-authored-by: Allen Householder <adh@cert.org>

--- src/ssvc/_mixins.py
+++ src/ssvc/_mixins.py
@@ -22,6 +22,7 @@ from typing import Optional
 
 from dataclasses_json import config, dataclass_json
 
+from . import _schemaVersion
 
 @dataclass_json
 @dataclass(kw_only=True)
@@ -31,7 +32,7 @@ class _Versioned:
     """
 
     version: str = "0.0.0"
-
+    schemaVersion: str = _schemaVersion
 
 @dataclass_json
 @dataclass(kw_only=True)
