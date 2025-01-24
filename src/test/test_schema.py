710abaddc498edc1f7be7ef30315ef45108ccb90
Allen D. Householder <adh@cert.org>
2025-01-24 16:25:49 -0500

    black reformat

--- src/test/test_schema.py
+++ src/test/test_schema.py
@@ -22,7 +22,6 @@ from referencing import Registry, Resource
 
 import ssvc.decision_points  # noqa F401
 from ssvc.decision_points.base import REGISTERED_DECISION_POINTS
-
 # importing these causes the decision points to register themselves
 from ssvc.decision_points.critical_software import CRITICAL_SOFTWARE_1  # noqa
 from ssvc.decision_points.high_value_asset import HIGH_VALUE_ASSET_1  # noqa
@@ -33,7 +32,6 @@ from ssvc.dp_groups.cvss.collections import (
     CVSSv3,
     CVSSv4,
 )  # noqa
-
 # importing these causes the decision points to register themselves
 from ssvc.dp_groups.ssvc.collections import SSVCv1, SSVCv2, SSVCv2_1  # noqa
 
@@ -94,15 +92,13 @@ class MyTestCase(unittest.TestCase):
             loaded = json.loads(as_json)
 
             try:
-                Draft202012Validator(
-                    {"$ref": schema_url}, registry=registry
-                ).validate(loaded)
+                Draft202012Validator({"$ref": schema_url}, registry=registry).validate(
+                    loaded
+                )
             except jsonschema.exceptions.ValidationError as e:
                 exp = e
 
-            self.assertIsNone(
-                exp, f"Validation failed for {dp.name} {dp.version}"
-            )
+            self.assertIsNone(exp, f"Validation failed for {dp.name} {dp.version}")
             self.logger.debug(
                 f"Validation passed for Decision Point ({dp.namespace}) {dp.name} v{dp.version}"
             )
@@ -115,15 +111,13 @@ class MyTestCase(unittest.TestCase):
             loaded = json.loads(as_json)
 
             try:
-                Draft202012Validator(
-                    {"$ref": schema_url}, registry=registry
-                ).validate(loaded)
+                Draft202012Validator({"$ref": schema_url}, registry=registry).validate(
+                    loaded
+                )
             except jsonschema.exceptions.ValidationError as e:
                 exp = e
 
-            self.assertIsNone(
-                exp, f"Validation failed for {dpg.name} {dpg.version}"
-            )
+            self.assertIsNone(exp, f"Validation failed for {dpg.name} {dpg.version}")
             self.logger.debug(
                 f"Validation passed for Decision Point Group {dpg.name} v{dpg.version}"
             )
