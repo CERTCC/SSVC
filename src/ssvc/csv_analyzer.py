85af2096c6d9eb8f0c158773bc9beaf2cd82b7c2
Allen D. Householder <adh@cert.org>
2025-03-03 15:15:30 -0500

    black reformat

--- src/ssvc/csv_analyzer.py
+++ src/ssvc/csv_analyzer.py
@@ -67,6 +67,9 @@ from sklearn.tree import DecisionTreeClassifier
 
 logger = logging.getLogger(__name__)
 
+# set an option to avoid a deprecation warning
+pd.set_option("future.no_silent_downcasting", True)
+
 
 def _col_norm(c: str) -> str:
     """
@@ -95,9 +98,7 @@ def _imp_df(column_names: list, importances: list) -> pd.DataFrame:
         a dataframe of feature importances
     """
     df = (
-        pd.DataFrame(
-            {"feature": column_names, "feature_importance": importances}
-        )
+        pd.DataFrame({"feature": column_names, "feature_importance": importances})
         .sort_values("feature_importance", ascending=False)
         .reset_index(drop=True)
     )
@@ -186,9 +187,7 @@ def _perm_feat_imp(model, x, y):
 
 def _parse_args(args) -> argparse.Namespace:
     # parse command line
-    parser = argparse.ArgumentParser(
-        description="Analyze an SSVC tree csv file"
-    )
+    parser = argparse.ArgumentParser(description="Analyze an SSVC tree csv file")
     parser.add_argument(
         "csvfile", metavar="csvfile", type=str, help="the csv file to analyze"
     )
@@ -375,12 +374,8 @@ def check_topological_order(df, target):
     for u in H.nodes:
         H.nodes[u]["outcome"] = G.nodes[u]["outcome"]
 
-    logger.debug(
-        f"Original graph: {len(G.nodes)} nodes with {len(G.edges)} edges"
-    )
-    logger.debug(
-        f"Reduced graph: {len(H.nodes)} nodes with {len(H.edges)} edges"
-    )
+    logger.debug(f"Original graph: {len(G.nodes)} nodes with {len(G.edges)} edges")
+    logger.debug(f"Reduced graph: {len(H.nodes)} nodes with {len(H.edges)} edges")
 
     problems = []
     # check if the outcome is topologically sorted
