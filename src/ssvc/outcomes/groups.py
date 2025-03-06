6f2cbc648872d0a279688a12eafcab406110f36e
Allen D. Householder <adh@cert.org>
2025-03-06 12:09:55 -0500

    carve a new version of CISA OutcomeGroup to reflect `Track *` -> `Monitor`

--- src/ssvc/outcomes/groups.py
+++ src/ssvc/outcomes/groups.py
@@ -107,7 +107,7 @@ CVSS = OutcomeGroup(
 The CVSS outcome group.
 """
 
-CISA = OutcomeGroup(
+CISA_1 = OutcomeGroup(
     name="CISA Levels",
     description="The CISA outcome group. "
     "CISA uses its own SSVC decision tree model to prioritize relevant vulnerabilities into four possible decisions: Track, Track*, Attend, and Act.",
@@ -122,7 +122,7 @@ CISA = OutcomeGroup(
         ),
         OutcomeValue(
             name="Track*",
-            key="T*",
+            key="R",
             description="The vulnerability contains specific characteristics that may require closer monitoring for changes. "
             "CISA recommends remediating Track* vulnerabilities within standard update timelines.",
         ),
@@ -135,7 +135,48 @@ CISA = OutcomeGroup(
         ),
         OutcomeValue(
             name="Act",
+            key="C",
+            description="The vulnerability requires attention from the organization's internal, supervisory-level and leadership-level individuals. "
+            "Necessary actions include requesting assistance or information about the vulnerability, as well as publishing a notification either internally and/or externally. "
+            "Typically, internal groups would meet to determine the overall response and then execute agreed upon actions. "
+            "CISA recommends remediating Act vulnerabilities as soon as possible.",
+        ),
+    ),
+)
+"""
+The CISA outcome group. Based on CISA's customizations of the SSVC model.
+See https://www.cisa.gov/stakeholder-specific-vulnerability-categorization-ssvc
+"""
+
+CISA = OutcomeGroup(
+    name="CISA Levels",
+    description="The CISA outcome group. "
+    "CISA uses its own SSVC decision tree model to prioritize relevant vulnerabilities into four possible decisions: Track, Monitor, Attend, and Act.",
+    version="1.1.0",
+    outcomes=(
+        OutcomeValue(
+            name="Track",
+            key="T",
+            description="The vulnerability does not require action at this time. "
+            "The organization would continue to track the vulnerability and reassess it if new information becomes available. "
+            "CISA recommends remediating Track vulnerabilities within standard update timelines.",
+        ),
+        OutcomeValue(
+            name="Monitor",
+            key="M",
+            description="The vulnerability contains specific characteristics that may require closer monitoring for changes. "
+            "CISA recommends remediating Track* vulnerabilities within standard update timelines.",
+        ),
+        OutcomeValue(
+            name="Attend",
             key="A",
+            description="The vulnerability requires attention from the organization's internal, supervisory-level individuals. "
+            "Necessary actions may include requesting assistance or information about the vulnerability and may involve publishing a notification, either internally and/or externally, about the vulnerability. "
+            "CISA recommends remediating Attend vulnerabilities sooner than standard update timelines.",
+        ),
+        OutcomeValue(
+            name="Act",
+            key="C",
             description="The vulnerability requires attention from the organization's internal, supervisory-level and leadership-level individuals. "
             "Necessary actions include requesting assistance or information about the vulnerability, as well as publishing a notification either internally and/or externally. "
             "Typically, internal groups would meet to determine the overall response and then execute agreed upon actions. "
