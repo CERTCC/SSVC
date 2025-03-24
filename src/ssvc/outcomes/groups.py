daddce30166b5ab0fa1d8a526cba886abe0585b3
Allen D. Householder <adh@cert.org>
2025-03-24 13:18:58 -0400

    adds a cisa namespace, CisaDecisionPoint base class, and outcome groups for CISA-specific decision models

--- src/ssvc/outcomes/groups.py
+++ src/ssvc/outcomes/groups.py
@@ -55,48 +55,6 @@ The Eisenhower outcome group.
 """
 
 
-CISA = SsvcDecisionPoint(
-    name="CISA Levels",
-    key="CISA",
-    description="The CISA outcome group. "
-    "CISA uses its own SSVC decision tree model to prioritize relevant vulnerabilities into four possible decisions: Track, Track*, Attend, and Act.",
-    version="1.0.0",
-    values=(
-        DecisionPointValue(
-            name="Track",
-            key="T",
-            description="The vulnerability does not require action at this time. "
-            "The organization would continue to track the vulnerability and reassess it if new information becomes available. "
-            "CISA recommends remediating Track vulnerabilities within standard update timelines.",
-        ),
-        DecisionPointValue(
-            name="Track*",
-            key="T*",
-            description="The vulnerability contains specific characteristics that may require closer monitoring for changes. "
-            "CISA recommends remediating Track* vulnerabilities within standard update timelines.",
-        ),
-        DecisionPointValue(
-            name="Attend",
-            key="A",
-            description="The vulnerability requires attention from the organization's internal, supervisory-level individuals. "
-            "Necessary actions may include requesting assistance or information about the vulnerability and may involve publishing a notification, either internally and/or externally, about the vulnerability. "
-            "CISA recommends remediating Attend vulnerabilities sooner than standard update timelines.",
-        ),
-        DecisionPointValue(
-            name="Act",
-            key="A",
-            description="The vulnerability requires attention from the organization's internal, supervisory-level and leadership-level individuals. "
-            "Necessary actions include requesting assistance or information about the vulnerability, as well as publishing a notification either internally and/or externally. "
-            "Typically, internal groups would meet to determine the overall response and then execute agreed upon actions. "
-            "CISA recommends remediating Act vulnerabilities as soon as possible.",
-        ),
-    ),
-)
-"""
-The CISA outcome group. Based on CISA's customizations of the SSVC model.
-See https://www.cisa.gov/stakeholder-specific-vulnerability-categorization-ssvc
-"""
-
 YES_NO = SsvcDecisionPoint(
     name="Yes, No",
     key="YES_NO",
