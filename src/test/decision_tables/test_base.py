eb59f46f0becb2da275929c00246bfc6f4f7798b
Allen D. Householder <adh@cert.org>
2025-03-25 16:45:27 -0400

    update tests

--- src/test/decision_tables/test_base.py
+++ src/test/decision_tables/test_base.py
@@ -1,25 +1,37 @@
-#  Copyright (c) 2025 Carnegie Mellon University and Contributors.
-#  - see Contributors.md for a full list of Contributors
-#  - see ContributionInstructions.md for information on how you can Contribute to this project
-#  Stakeholder Specific Vulnerability Categorization (SSVC) is
-#  licensed under a MIT (SEI)-style license, please see LICENSE.md distributed
-#  with this Software or contact permission@sei.cmu.edu for full terms.
-#  Created, in part, with funding and support from the United States Government
-#  (see Acknowledgments file). This program may include and/or can make use of
-#  certain third party source code, object code, documentation and other files
-#  (“Third Party Software”). See LICENSE.md for more details.
-#  Carnegie Mellon®, CERT® and CERT Coordination Center® are registered in the
-#  U.S. Patent and Trademark Office by Carnegie Mellon University
+#  Copyright (c) 2025 Carnegie Mellon University.
+#  NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE
+#  ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS.
+#  CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND,
+#  EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT
+#  NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR
+#  MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE
+#  OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE
+#  ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM
+#  PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+#  Licensed under a MIT (SEI)-style license, please see LICENSE or contact
+#  permission@sei.cmu.edu for full terms.
+#  [DISTRIBUTION STATEMENT A] This material has been approved for
+#  public release and unlimited distribution. Please see Copyright notice
+#  for non-US Government use and distribution.
+#  This Software includes and/or makes use of Third-Party Software each
+#  subject to its own license.
+#  DM24-0278
 import tempfile
 import unittest
 
-from ssvc.decision_points.base import DecisionPoint, DecisionPointValue
+from pandas import DataFrame
+
+from ssvc.decision_points.base import (
+    DecisionPoint,
+    DecisionPointValue,
+    _DECISION_POINT_REGISTRY,
+)
 from ssvc.decision_tables import base
 from ssvc.dp_groups.base import DecisionPointGroup
 from ssvc.outcomes.base import OutcomeValue
 
 
-class TestDecisionTables(unittest.TestCase):
+class TestDecisionTable(unittest.TestCase):
     def setUp(self):
         self.tempdir = tempfile.TemporaryDirectory()
         self.tempdir_path = self.tempdir.name
@@ -86,6 +98,20 @@ class TestDecisionTables(unittest.TestCase):
         self.assertEqual(self.dt.namespace, "x_test")
         self.assertEqual(self.dt.decision_point_group, self.dpg)
         self.assertEqual(self.dt.outcome_group, self.og)
+
+    def test_get_mapping_df(self):
+        df = self.dt.get_mapping_df()
+        self.assertIsInstance(df, DataFrame)
+        # columns are the decision point strings and the outcome group string
+        for dp in self.dpg.decision_points:
+            self.assertIn(dp.str, df.columns[:-1])
+        self.assertEqual(self.og.str, df.columns[-1])
+
+        for col in df.columns:
+            dp = _DECISION_POINT_REGISTRY[col]
+            # all values in the decision point should be in the column at least once
+            for vsum in dp.value_summaries_str:
+                self.assertIn(vsum, df[col].unique())
 
 
 if __name__ == "__main__":
