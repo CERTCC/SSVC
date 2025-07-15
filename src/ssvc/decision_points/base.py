82947e2dad79d6cc6af33e2d3fed5c4dab7a5aa8
Allen D. Householder <adh@cert.org>
2025-07-15 16:45:30 -0400

    we don't need decision point id

--- src/ssvc/decision_points/base.py
+++ src/ssvc/decision_points/base.py
@@ -188,10 +188,6 @@ class DecisionPoint(
         return FIELD_DELIMITER.join([self.namespace, self.key, self.version])
 
     @property
-    def id(self):
-        return ":".join([self.namespace, self.key, self.version])
-
-    @property
     def str(self) -> str:
         """
         Return the DecisionPoint represented as a short string.
