25b883eb103f32caeaf6a8c380240c4d52a70ac9
Allen D. Householder <adh@cert.org>
2025-07-18 14:15:42 -0400

    try out the decision table object with a few known tables

--- src/ssvc/decision_tables/ssvc/utlity.py
+++ src/ssvc/decision_tables/ssvc/utlity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 """
-Models the human impact decision table for SSVC.
+Models the Utility decision table for SSVC.
 """
+
 #  Copyright (c) 2025 Carnegie Mellon University.
 #  NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE
 #  ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS.
@@ -21,9 +22,60 @@ Models the human impact decision table for SSVC.
 #  subject to its own license.
 #  DM24-0278
 
+from ssvc.decision_points.ssvc.automatable import LATEST as Automatable
+from ssvc.decision_points.ssvc.utility import LATEST as Utility
+from ssvc.decision_points.ssvc.value_density import LATEST as ValueDensity
+from ssvc.decision_tables.base import DecisionTable, decision_table_to_longform_df
+from ssvc.decision_tables.helpers import write_csv
+
+UTILITY_1 = DecisionTable(
+        name='Utility',
+        namespace ='ssvc',
+        description='Utility decision table for SSVC',
+        version='1.0.0',
+        decision_points={dp.id: dp for dp in [Automatable,ValueDensity,Utility]},
+        outcome = Utility.id,
+        mapping =  [
+            {
+              "ssvc:A:2.0.0": "N",
+              "ssvc:VD:1.0.0": "D",
+              "ssvc:U:1.0.1": "L"
+            },
+            {
+              "ssvc:A:2.0.0": "N",
+              "ssvc:VD:1.0.0": "C",
+              "ssvc:U:1.0.1": "E"
+            },
+            {
+              "ssvc:A:2.0.0": "Y",
+              "ssvc:VD:1.0.0": "D",
+              "ssvc:U:1.0.1": "E"
+            },
+            {
+              "ssvc:A:2.0.0": "Y",
+              "ssvc:VD:1.0.0": "C",
+              "ssvc:U:1.0.1": "S"
+            }
+          ]
+)
+
+VERSIONS = (UTILITY_1,)
+LATEST = VERSIONS[-1]
+
 
 def main():
-    pass
+    print("## Utility Decision Table Object")
+    print()
+    print(UTILITY_1.model_dump_json(indent=2))
+
+    print("## Utility Decision Table Longform DataFrame CSV")
+    print()
+    print(decision_table_to_longform_df(UTILITY_1).to_csv(index=False))
+
+    csvfile = "utility.csv"
+    write_csv(UTILITY_1,csvfile)
+
+
 
 
 if __name__ == '__main__':
