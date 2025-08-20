253463396489b9a105ee42ea2b9db6ecaabe56ba
Allen D. Householder <adh@cert.org>
2025-08-20 15:54:07 -0400

    refactor

--- src/ssvc/api.py
+++ src/ssvc/api.py
@@ -23,33 +23,26 @@ API for SSVC
 #  subject to its own license.
 #  DM24-0278
 
-from typing import Any, Dict
+from typing import Any
 
 from fastapi import FastAPI, HTTPException
-from pydantic import RootModel
 
+import ssvc  # noqa: F401
 from ssvc.decision_points.base import DecisionPoint, DecisionPointValue
 from ssvc.decision_tables.base import DecisionTable
 from ssvc.registry import get_registry
 from ssvc.registry.base import (
     lookup_key,
+    lookup_latest,
     lookup_namespace,
     lookup_objtype,
     lookup_version,
 )
+from ssvc.utils.api_helpers import DecisionPointDict, DecisionTableDict
 
 app = FastAPI()
 
 r = get_registry()
-
-
-# TODO: move convenience object models to a separate module
-class DecisionPointDict(RootModel[Dict[str, DecisionPoint]]):
-    """A dictionary of DecisionPoint objects with keys as 'namespace:key:version'."""
-
-
-class DecisionTableDict(RootModel[Dict[str, DecisionTable]]):
-    """A dictionary of DecisionTable objects with keys as 'namespace:key:version'."""
 
 
 @app.get("/")
@@ -63,7 +56,7 @@ def _404_on_none(obj: Any):
 
 
 @app.get("/decision_points", response_model=DecisionPointDict)
-async def get_decision_points() -> DecisionPointDict:
+async def get_all_decision_points() -> DecisionPointDict:
     result = lookup_objtype(objtype="DecisionPoint", registry=r)
     _404_on_none(result)
 
@@ -78,7 +71,9 @@ async def get_decision_points() -> DecisionPointDict:
 
 
 @app.get("/decision_points/{namespace}", response_model=DecisionPointDict)
-async def get_decision_points(namespace: str) -> DecisionPointDict:
+async def get_all_decision_points_for_namespace(
+    namespace: str,
+) -> DecisionPointDict:
     result = lookup_namespace(
         objtype="DecisionPoint", namespace=namespace, registry=r
     )
@@ -96,7 +91,9 @@ async def get_decision_points(namespace: str) -> DecisionPointDict:
 @app.get(
     "/decision_points/{namespace}/{key}", response_model=DecisionPointDict
 )
-async def get_decision_points(namespace: str, key: str) -> DecisionPointDict:
+async def get_all_versions_of_decision_points_for_key(
+    namespace: str, key: str
+) -> DecisionPointDict:
     """Returns a dictionary of DecisionPoint objects for the given namespace and key.
     Dictionary keys are namespace:key:version."""
     result = lookup_key(
@@ -112,10 +109,26 @@ async def get_decision_points(namespace: str, key: str) -> DecisionPointDict:
 
 
 @app.get(
+    "/decision_points/{namespace}/{key}/latest",
+    response_model=DecisionPoint,
+)
+async def get_latest_decision_point_for_key(
+    namespace: str, key: str
+) -> DecisionPoint:
+    """Returns the latest DecisionPoint object for the given namespace and key."""
+    result = lookup_latest(
+        objtype="DecisionPoint", namespace=namespace, key=key, registry=r
+    )
+    _404_on_none(result)
+    dp = result
+    return dp
+
+
+@app.get(
     "/decision_points/{namespace}/{key}/{version}",
     response_model=DecisionPoint,
 )
-async def get_decision_points(
+async def get_decision_point_version(
     namespace: str, key: str, version: str
 ) -> DecisionPoint:
     """Returns a single DecisionPoint object for the given namespace, key, and version."""
@@ -135,10 +148,10 @@ async def get_decision_points(
     "/decision_points/{namespace}/{key}/{version}/values",
     response_model=list[DecisionPointValue],
 )
-async def get_decision_points(
+async def get_decision_point_values(
     namespace: str, key: str, version: str
 ) -> DecisionPoint:
-    """Returns a single DecisionPoint object for the given namespace, key, and version."""
+    """Returns the values of a single DecisionPoint object for the given namespace, key, and version."""
     result = lookup_version(
         objtype="DecisionPoint",
         namespace=namespace,
@@ -167,7 +180,7 @@ async def get_decision_tables() -> DecisionTableDict:
 
 
 @app.get("/decision_tables/{namespace}", response_model=DecisionTableDict)
-async def get_decision_tables_namespace(
+async def get_decision_tables_for_namespace(
     namespace: str,
 ) -> DecisionTableDict:
 
@@ -177,8 +190,8 @@ async def get_decision_tables_namespace(
     _404_on_none(ns_obj)
     # namespace obj has keys, keys have versions.
     objs = {}
-    for k in ns_obj.keys:
-        for ver in ns_obj.keys[k].versions:
+    for k in ns_obj.keys.keys():
+        for ver in ns_obj.keys[k].versions.keys():
             obj = ns_obj.keys[k].versions[ver].obj
             objs[obj.id] = obj
     return DecisionTableDict(**objs)
@@ -187,7 +200,7 @@ async def get_decision_tables_namespace(
 @app.get(
     "/decision_tables/{namespace}/{key}", response_model=DecisionTableDict
 )
-async def get_decision_tables_key(
+async def get_decision_tables_for_key(
     namespace: str, key: str
 ) -> DecisionTableDict:
     """Returns a dictionary of DecisionTable objects for the given namespace and key.
@@ -208,10 +221,26 @@ async def get_decision_tables_key(
 
 
 @app.get(
+    "/decision_tables/{namespace}/{key}/latest",
+    response_model=DecisionTable,
+)
+async def get_latest_decision_table_for_key(
+    namespace: str, key: str
+) -> DecisionTable:
+    """Returns the latest DecisionPoint object for the given namespace and key."""
+    result = lookup_latest(
+        objtype="DecisionTable", namespace=namespace, key=key, registry=r
+    )
+    _404_on_none(result)
+    dt = result
+    return dt
+
+
+@app.get(
     "/decision_tables/{namespace}/{key}/{version}",
     response_model=DecisionTable,
 )
-async def get_decision_tables_version(
+async def get_decision_table_version(
     namespace: str, key: str, version: str
 ) -> DecisionTable:
     """Returns a single DecisionTable object for the given namespace, key, and version."""
@@ -228,7 +257,7 @@ async def get_decision_tables_version(
 
 
 @app.get("/namespaces", response_model=list[str])
-def get_namespaces() -> list[str]:
+def get_namespace_list() -> list[str]:
     namespaces = set()
     for objtype in r.types:
         for namespace in r.types[objtype].namespaces:
@@ -237,13 +266,13 @@ def get_namespaces() -> list[str]:
 
 
 @app.get("/types", response_model=list[str])
-def get_types() -> list[str]:
+def get_type_list() -> list[str]:
     """Returns a list of all object types in the registry."""
     return sorted(list(r.types.keys()))
 
 
 @app.get("/namespaces/{type}", response_model=list[str])
-def get_types_namespaces(type: str) -> list[str]:
+def get_namespace_list_for_type(type: str) -> list[str]:
     """Returns a list of all namespaces for a given object type in the registry."""
     objtype = lookup_objtype(objtype=type, registry=r)
     _404_on_none(objtype)
@@ -251,7 +280,7 @@ def get_types_namespaces(type: str) -> list[str]:
 
 
 @app.get("/keys/{type}/{namespace}", response_model=list[str])
-def get_keys(type: str, namespace: str) -> list[str]:
+def get_key_list_for_namespace(type: str, namespace: str) -> list[str]:
     """Returns a list of all keys for a given object type and namespace in the registry."""
     ns = lookup_namespace(objtype=type, namespace=namespace, registry=r)
     _404_on_none(ns)
@@ -259,7 +288,7 @@ def get_keys(type: str, namespace: str) -> list[str]:
 
 
 @app.get("/versions/{type}/{namespace}/{key}", response_model=list[str])
-def get_versions(type: str, namespace: str, key: str) -> list[str]:
+def get_version_list_for_key(type: str, namespace: str, key: str) -> list[str]:
     """Returns a list of all versions for a given object type, namespace, and key in the registry."""
     k = lookup_key(objtype=type, namespace=namespace, key=key, registry=r)
     _404_on_none(k)
---
+++ src/ssvc/utils/api_helpers.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+"""
+file: api_helpers
+author: adh
+created_at: 8/20/25 3:22 PM
+"""
+
+#  Copyright (c) 2025 Carnegie Mellon University.
+#  NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE
+#  ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS.
+#  CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND,
+#  EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT
+#  NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR
+#  MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE
+#  OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE
+#  ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM
+#  PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+#  Licensed under a MIT (SEI)-style license, please see LICENSE or contact
+#  permission@sei.cmu.edu for full terms.
+#  [DISTRIBUTION STATEMENT A] This material has been approved for
+#  public release and unlimited distribution. Please see Copyright notice
+#  for non-US Government use and distribution.
+#  This Software includes and/or makes use of Third-Party Software each
+#  subject to its own license.
+#  DM24-0278
+
+from typing import Dict
+
+from pydantic import RootModel
+
+from ssvc.decision_points.base import DecisionPoint
+from ssvc.decision_tables.base import DecisionTable
+
+
+#  Copyright (c) 2025 Carnegie Mellon University.
+#  NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE
+#  ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS.
+#  CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND,
+#  EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT
+#  NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR
+#  MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE
+#  OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE
+#  ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM
+#  PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+#  Licensed under a MIT (SEI)-style license, please see LICENSE or contact
+#  permission@sei.cmu.edu for full terms.
+#  [DISTRIBUTION STATEMENT A] This material has been approved for
+#  public release and unlimited distribution. Please see Copyright notice
+#  for non-US Government use and distribution.
+#  This Software includes and/or makes use of Third-Party Software each
+#  subject to its own license.
+#  DM24-0278
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+class DecisionPointDict(RootModel[Dict[str, DecisionPoint]]):
+    """A dictionary of DecisionPoint objects with keys as 'namespace:key:version'."""
+
+
+class DecisionTableDict(RootModel[Dict[str, DecisionTable]]):
+    """A dictionary of DecisionTable objects with keys as 'namespace:key:version'."""
