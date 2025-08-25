12abad300455dfde2f20156d5ed68af77022df73
Allen D. Householder <adh@cert.org>
2025-08-25 11:32:58 -0400

    add reference docs for SSVC and CVSS collections decision point groups.

--- src/ssvc/dp_groups/cvss/collections.py
+++ src/ssvc/dp_groups/cvss/collections.py
@@ -132,7 +132,7 @@ from ssvc.decision_points.cvss.user_interaction import (
 from ssvc.dp_groups.base import DecisionPointGroup
 
 # Instantiate the CVSS v1 decision point group
-_BASE_1 = [
+BASE_1 = [
     ACCESS_VECTOR_1,
     ACCESS_COMPLEXITY_1,
     AUTHENTICATION_1,
@@ -141,42 +141,50 @@ _BASE_1 = [
     AVAILABILITY_IMPACT_1,
     IMPACT_BIAS_1,
 ]
-_TEMPORAL_1 = [
+"""List of CVSS v1 Base decision points"""
+
+TEMPORAL_1 = [
     EXPLOITABILITY_1,
     REMEDIATION_LEVEL_1,
     REPORT_CONFIDENCE_1,
 ]
-_ENVIRONMENTAL_1 = [
+"""List of CVSS v1 Temporal decision points"""
+
+ENVIRONMENTAL_1 = [
     COLLATERAL_DAMAGE_POTENTIAL_1,
     TARGET_DISTRIBUTION_1,
 ]
+"""List of CVSS v1 Environmental decision points"""
 
 CVSSv1_B = DecisionPointGroup(
     name="CVSS",
     version="1.0.0",
     description="CVSS v1 decision points",
-    decision_points=tuple(_BASE_1),
+    decision_points=tuple(BASE_1),
 )
+"""CVSS v1 Base Metrics"""
 
 CVSSv1_BT = DecisionPointGroup(
     name="CVSS",
     version="1.0.0",
     description="CVSS v1 decision points",
-    decision_points=tuple(_BASE_1 + _TEMPORAL_1),
+    decision_points=tuple(BASE_1 + TEMPORAL_1),
 )
+"""CVSS v1 Base and Temporal Metrics"""
 
 CVSSv1_BTE = DecisionPointGroup(
     name="CVSS",
     version="1.0.0",
     description="CVSS v1 decision points",
-    decision_points=tuple(_BASE_1 + _TEMPORAL_1 + _ENVIRONMENTAL_1),
+    decision_points=tuple(BASE_1 + TEMPORAL_1 + ENVIRONMENTAL_1),
 )
+"""CVSS v1 Base, Temporal, and Environmental Metrics"""
 
 CVSSv1 = CVSSv1_BTE  # convenience alias
 
 # CVSS v2 decision points
 
-_BASE_2 = [
+BASE_2 = [
     ACCESS_VECTOR_2,
     ACCESS_COMPLEXITY_2,
     AUTHENTICATION_2,
@@ -184,44 +192,52 @@ _BASE_2 = [
     INTEGRITY_IMPACT_1,
     AVAILABILITY_IMPACT_1,
 ]
-_TEMPORAL_2 = [
+"""List of CVSS v2 Base decision points"""
+
+TEMPORAL_2 = [
     EXPLOITABILITY_1_1,
     REMEDIATION_LEVEL_1_1,
     REPORT_CONFIDENCE_1_1,
 ]
-_ENVIRONMENTAL_2 = [
+"""List of CVSS v2 Temporal decision points"""
+
+ENVIRONMENTAL_2 = [
     COLLATERAL_DAMAGE_POTENTIAL_2,
     TARGET_DISTRIBUTION_1_1,
     CONFIDENTIALITY_REQUIREMENT_1,
     INTEGRITY_REQUIREMENT_1,
     AVAILABILITY_REQUIREMENT_1,
 ]
+"""List of CVSS v2 Environmental decision points"""
 
 CVSSv2_B = DecisionPointGroup(
     name="CVSS Version 2 Base Metrics",
     description="Base metrics for CVSS v2",
     version="2.0.0",
-    decision_points=tuple(_BASE_2),
+    decision_points=tuple(BASE_2),
 )
+"""CVSS v2 Base Metrics"""
 
 CVSSv2_BT = DecisionPointGroup(
     name="CVSS Version 2 Base and Temporal Metrics",
     description="Base and Temporal metrics for CVSS v2",
     version="2.0.0",
-    decision_points=tuple(_BASE_2 + _TEMPORAL_2),
+    decision_points=tuple(BASE_2 + TEMPORAL_2),
 )
+"""CVSS v2 Base and Temporal Metrics"""
 
 CVSSv2_BTE = DecisionPointGroup(
     name="CVSS Version 2 Base, Temporal, and Environmental Metrics",
     description="Base, Temporal, and Environmental metrics for CVSS v2",
     version="2.0.0",
-    decision_points=tuple(_BASE_2 + _TEMPORAL_2 + _ENVIRONMENTAL_2),
+    decision_points=tuple(BASE_2 + TEMPORAL_2 + ENVIRONMENTAL_2),
 )
+"""CVSS v2 Base, Temporal, and Environmental Metrics"""
 
 CVSSv2 = CVSSv2_BTE  # convenience alias
 
 # CVSS v3 decision points
-_BASE_3 = [
+BASE_3 = [
     ATTACK_VECTOR_3,
     ATTACK_COMPLEXITY_3,
     PRIVILEGES_REQUIRED_1,
@@ -231,54 +247,61 @@ _BASE_3 = [
     INTEGRITY_IMPACT_2,
     AVAILABILITY_IMPACT_2,
 ]
-_TEMPORAL_3 = [
+"""List of CVSS v3 Base decision points"""
+
+TEMPORAL_3 = [
     EXPLOIT_CODE_MATURITY_1_2,
     REMEDIATION_LEVEL_1_1,
     REPORT_CONFIDENCE_2,
 ]
+"""List of CVSS v3 Temporal decision points"""
 
-_ENVIRONMENTAL_3 = [modify_3(dp) for dp in _BASE_3]
-_ENVIRONMENTAL_3.extend(
-    [
-        CONFIDENTIALITY_REQUIREMENT_1_1,
-        INTEGRITY_REQUIREMENT_1_1,
-        AVAILABILITY_REQUIREMENT_1_1,
-    ]
-)
+ENVIRONMENTAL_3 = [modify_3(dp) for dp in BASE_3] + [
+    CONFIDENTIALITY_REQUIREMENT_1_1,
+    INTEGRITY_REQUIREMENT_1_1,
+    AVAILABILITY_REQUIREMENT_1_1,
+]
+"""List of CVSS v3 Environmental decision points"""
+
 
 CVSSv3_B = DecisionPointGroup(
     name="CVSS Version 3 Base Metrics",
     description="Base metrics for CVSS v3",
     version="3.0.0",
-    decision_points=tuple(_BASE_3),
+    decision_points=tuple(BASE_3),
 )
+"""CVSS v3 Base Metrics"""
 
 CVSSv3_BT = DecisionPointGroup(
     name="CVSS Version 3 Base and Temporal Metrics",
     description="Base and Temporal metrics for CVSS v3",
     version="3.0.0",
-    decision_points=tuple(_BASE_3 + _TEMPORAL_3),
+    decision_points=tuple(BASE_3 + TEMPORAL_3),
 )
+"""CVSS v3 Base and Temporal Metrics"""
 
 CVSSv3_BTE = DecisionPointGroup(
     name="CVSS Version 3 Base, Temporal, and Environmental Metrics",
     description="Base, Temporal, and Environmental metrics for CVSS v3",
     version="3.0.0",
-    decision_points=tuple(_BASE_3 + _TEMPORAL_3 + _ENVIRONMENTAL_3),
+    decision_points=tuple(BASE_3 + TEMPORAL_3 + ENVIRONMENTAL_3),
 )
+"""CVSS v3 Base, Temporal, and Environmental Metrics"""
 
 CVSSv3 = CVSSv3_BTE  # convenience alias
 
 # CVSS v4 decision points
 
-_EXPLOITABILITY_4 = [
+EXPLOITABILITY_4 = [
     ATTACK_VECTOR_3_0_1,
     ATTACK_COMPLEXITY_3_0_1,
     ATTACK_REQUIREMENTS_1,
     PRIVILEGES_REQUIRED_1_0_1,
     USER_INTERACTION_2,
 ]
-_IMPACT_4 = [
+"""List of CVSS v4 Exploitability decision points"""
+
+IMPACT_4 = [
     CONFIDENTIALITY_IMPACT_3_0_0,
     INTEGRITY_IMPACT_3_0_0,
     AVAILABILITY_IMPACT_3_0_0,
@@ -286,22 +309,25 @@ _IMPACT_4 = [
     SUBSEQUENT_INTEGRITY_IMPACT_1,
     SUBSEQUENT_AVAILABILITY_IMPACT_1,
 ]
-_BASE_4 = _EXPLOITABILITY_4 + _IMPACT_4
+"""List of CVSS v4 Impact decision points"""
+
+BASE_4 = EXPLOITABILITY_4 + IMPACT_4
+"""List of CVSS v4 Base decision points"""
 
 # note: CVSS v4 does more than just insert "modified" in front of the name and "M" in front of the key
-_ENVIRONMENTAL_4 = [modify_4(dp) for dp in _BASE_4]
-_ENVIRONMENTAL_4.extend(
-    [
-        CONFIDENTIALITY_REQUIREMENT_1_1_1,
-        INTEGRITY_REQUIREMENT_1_1_1,
-        AVAILABILITY_REQUIREMENT_1_1_1,
-    ]
-)
-_THREAT_4 = [
+ENVIRONMENTAL_4 = [modify_4(dp) for dp in BASE_4] + [
+    CONFIDENTIALITY_REQUIREMENT_1_1_1,
+    INTEGRITY_REQUIREMENT_1_1_1,
+    AVAILABILITY_REQUIREMENT_1_1_1,
+]
+"""List of CVSS v4 Environmental decision points"""
+
+THREAT_4 = [
     EXPLOIT_MATURITY_2,
 ]
+"""List of CVSS v4 Threat decision points"""
 
-_SUPPLEMENTAL_4 = [
+SUPPLEMENTAL_4 = [
     SAFETY_1,
     AUTOMATABLE_1,
     PROVIDER_URGENCY_1,
@@ -309,46 +335,53 @@ _SUPPLEMENTAL_4 = [
     VALUE_DENSITY_1,
     VULNERABILITY_RESPONSE_EFFORT_1,
 ]
+"""List of CVSS v4 Supplemental decision points"""
+
 # CVSS-B	Base metrics
 CVSSv4_B = DecisionPointGroup(
     name="CVSSv4 Base Metrics",
     description="Base metrics for CVSS v4",
     version="4.0.0",
-    decision_points=tuple(_BASE_4),
+    decision_points=tuple(BASE_4),
 )
+"""CVSS v4 Base Metrics"""
 
 # CVSS-BE	Base and Environmental metrics
 CVSSv4_BE = DecisionPointGroup(
     name="CVSSv4 Base and Environmental Metrics",
     description="Base and Environmental metrics for CVSS v4",
     version="4.0.0",
-    decision_points=tuple(_BASE_4 + _ENVIRONMENTAL_4),
+    decision_points=tuple(BASE_4 + ENVIRONMENTAL_4),
 )
+"""CVSS v4 Base and Environmental Metrics"""
 
 # CVSS-BT	Base and Threat metrics
 CVSSv4_BT = DecisionPointGroup(
     name="CVSSv4 Base and Threat Metrics",
     description="Base and Threat metrics for CVSS v4",
     version="4.0.0",
-    decision_points=tuple(_BASE_4 + _THREAT_4),
+    decision_points=tuple(BASE_4 + THREAT_4),
 )
+"""CVSS v4 Base and Threat Metrics"""
 
 # CVSS-BTE
 CVSSv4_BTE = DecisionPointGroup(
     name="CVSSv4 Base, Threat, and Environmental Metrics",
     description="Base, Threat, and Environmental metrics for CVSS v4",
     version="4.0.0",
-    decision_points=tuple(_BASE_4 + _THREAT_4 + _ENVIRONMENTAL_4),
+    decision_points=tuple(BASE_4 + THREAT_4 + ENVIRONMENTAL_4),
 )
+"""CVSS v4 Base, Threat, and Environmental Metrics"""
 
 CVSSv4 = DecisionPointGroup(
     name="CVSSv4",
     description="All decision points for CVSS v4 (including supplemental metrics)",
     version="4.0.0",
     decision_points=tuple(
-        _BASE_4 + _THREAT_4 + _ENVIRONMENTAL_4 + _SUPPLEMENTAL_4
+        BASE_4 + THREAT_4 + ENVIRONMENTAL_4 + SUPPLEMENTAL_4
     ),
 )
+"""CVSS v4 All Metrics"""
 
 CVSSv4_Equivalence_Sets = DecisionPointGroup(
     name="CVSSv4 EQ Sets",
@@ -363,6 +396,7 @@ CVSSv4_Equivalence_Sets = DecisionPointGroup(
         EQ6,
     ),
 )
+
 
 CVSSv4_EQ = CVSSv4_Equivalence_Sets  # convenience alias
 
