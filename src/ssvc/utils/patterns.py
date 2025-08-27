CERTCC/SSVC

commit 86b62f4ac6b42515e964a34a4a4a7510e7930353

improve ssvc_namespace_pattern.abnf (minor)
  * Use SPECIALCHAR instead of `[ "." / "-" ]`

src/ssvc/utils/ssvc_namespace_pattern.abnf
@@ -14,7 +14,7 @@ reg-base = ns-core [ "#" fragment-seg ]
 ; ns-core starts with a lowercase letter and may have '.' or '-' separators.
 ; Consecutive '.' or '-' are not allowed.
 ; It needs at least 3 characters.
-ns-core = LOWER ALNUMLOW 1*( [ "." / "-" ] 1*ALNUMLOW)
+ns-core = LOWER ALNUMLOW 1*( SPECIALCHAR 1*ALNUMLOW)
 
 x-name = reverse-dns "#" fragment-seg
 
@@ -25,7 +25,7 @@ reverse-dns = label 1*("." label)
 ; Each label must be between 1 and 63 characters long
 label = ALNUMLOW [ *61(ALNUMLOWDASH)  ALNUMLOW ]
 
-fragment-seg = 1*ALNUMLOW *( ("." / "-") 1*ALNUMLOW )
+fragment-seg = 1*ALNUMLOW *( SPECIALCHAR 1*ALNUMLOW )
 
 extensions = lang-ext [ 1*("/" ext-seg) ]
 

commit 14e65d001472a0b3f2258a1cd4e4efa678e12cb3

fix namespace ABNF and resulting pattern
  * correct defect introduced by 86b62f4 in the ABNF.
  * do minor cleanup in the ABNF: make whitespace consistent and remove
    superfluous brackets.
  * update the python pattern with the ABNF output, which leads
    to the same combined json schema pattern.

src/ssvc/utils/patterns.py
@@ -47,15 +47,14 @@ lower = '[a-z]'
 alnumlow = f'({lower}|[0-9])'
 dash = '-'
 alnumlowdash = f'({alnumlow}|{dash})'
-dot = '\\.'
-specialchar = f'({dot}|{dash})'
-alnumlowsc = f'({alnumlow}|{specialchar})'
 label = f'{alnumlow}(({alnumlowdash}){{,61}}{alnumlow})?'
 reverse_dns = f'{label}(\\.{label})+'
-fragment_seg = f'({alnumlow})+([.\\-]({alnumlow})+)*'
+dot = '\\.'
+specialchar = f'({dot}|{dash})'
+fragment_seg = f'({alnumlow})+({specialchar}({alnumlow})+)*'
 x_name = f'{reverse_dns}\\#{fragment_seg}'
 x_base = f'x_{x_name}'
-ns_core = f'{lower}{alnumlow}([.\\-]?({alnumlow})+)+'
+ns_core = f'{lower}{alnumlow}(({specialchar})?({alnumlow})+)+'
 reg_base = f'{ns_core}(\\#{fragment_seg})?'
 base_ns = f'({x_base}|{reg_base})'
 singleton = '[0-9A-WY-Za-wy-z]'

src/ssvc/utils/ssvc_namespace_pattern.abnf
@@ -14,7 +14,7 @@ reg-base = ns-core [ "#" fragment-seg ]
 ; ns-core starts with a lowercase letter and may have '.' or '-' separators.
 ; Consecutive '.' or '-' are not allowed.
 ; It needs at least 3 characters.
-ns-core = LOWER ALNUMLOW 1*( SPECIALCHAR 1*ALNUMLOW)
+ns-core = LOWER ALNUMLOW 1*( [ SPECIALCHAR ] 1*ALNUMLOW )
 
 x-name = reverse-dns "#" fragment-seg
 
@@ -23,16 +23,16 @@ x-name = reverse-dns "#" fragment-seg
 reverse-dns = label 1*("." label)
 
 ; Each label must be between 1 and 63 characters long
-label = ALNUMLOW [ *61(ALNUMLOWDASH)  ALNUMLOW ]
+label = ALNUMLOW [ *61ALNUMLOWDASH ALNUMLOW ]
 
 fragment-seg = 1*ALNUMLOW *( SPECIALCHAR 1*ALNUMLOW )
 
-extensions = lang-ext [ 1*("/" ext-seg) ]
+extensions = lang-ext [ 1*( "/" ext-seg ) ]
 
 ; Language extension: either / (empty language extension)
 ; or /<bcp47> (BCP-47 language code)
 
-lang-ext = "/" /  ( "/" bcp47 )
+lang-ext = "/" / ( "/" bcp47 )
 
 ; Extension segment between slashes.
 ; - is either a language or
@@ -48,7 +48,7 @@ bcp47 = ( 2*3ALPHA
         [ "-" 4ALPHA ]
         [ "-" ( 2ALPHA / 3DIGIT ) ]
         *( "-" ( 5*8ALNUM / (DIGIT 3ALNUM) ) )
-        *( "-"  singleton 1*("-" (2*8ALNUM)))
+        *( "-"  singleton 1*( "-" 2*8ALNUM ) )
         [ "-" "x" 1*( "-" 2*8ALNUM ) ]
       / "x" 1*( "-" 2*8ALNUM )
       / "i-default"