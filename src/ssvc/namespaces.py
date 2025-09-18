b575db625a5bf97ec415e6b56614cce2f828bf39
Allen D. Householder <adh@cert.org>
2025-09-18 13:20:08 -0400

    remove duplicate copyright

--- src/ssvc/namespaces.py
+++ src/ssvc/namespaces.py
@@ -4,7 +4,6 @@ SSVC objects use namespaces to distinguish between objects that arise from diffe
 stakeholders or analytical category sources. This module defines the official namespaces
 for SSVC and provides a method to validate namespace values.
 """
-
 #  Copyright (c) 2025 Carnegie Mellon University.
 #  NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE
 #  ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS.
@@ -29,25 +28,6 @@ from enum import StrEnum
 
 from ssvc.utils.defaults import MAX_NS_LENGTH, MIN_NS_LENGTH, X_PFX
 from ssvc.utils.patterns import NS_PATTERN_STR
-
-#  Copyright (c) 2025 Carnegie Mellon University.
-#  NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE
-#  ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS.
-#  CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND,
-#  EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT
-#  NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR
-#  MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE
-#  OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE
-#  ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM
-#  PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
-#  Licensed under a MIT (SEI)-style license, please see LICENSE or contact
-#  permission@sei.cmu.edu for full terms.
-#  [DISTRIBUTION STATEMENT A] This material has been approved for
-#  public release and unlimited distribution. Please see Copyright notice
-#  for non-US Government use and distribution.
-#  This Software includes and/or makes use of Third-Party Software each
-#  subject to its own license.
-#  DM24-0278
 
 EXT_SEP = "/"
 FRAG_SEP = "#"
